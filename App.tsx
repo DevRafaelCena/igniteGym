5895fa0d3e1e65cd8a9606a9e738c527a7b3d514
Rafael Cena <rafael.cena@hotmail.com>
2022-11-15 12:50:51 -0300

    finalizando signUp

--- App.tsx
+++ App.tsx
@@ -5,6 +5,7 @@ import {  NativeBaseProvider} from 'native-base'
 import { THEME } from './src/theme';
 import { Loading } from '@components/Loading';
 import { SignIn } from '@screens/Signin';
+import { SignUp } from '@screens/SignUp';
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
@@ -16,7 +17,7 @@ export default function App() {
         backgroundColor="transparent"
         translucent
       />
-      {fontsLoaded ? <SignIn /> : <Loading />}
+      {fontsLoaded ? <SignUp /> : <Loading />}
     </NativeBaseProvider>
   );
 }
