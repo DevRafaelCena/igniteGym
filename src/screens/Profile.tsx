dcf49710d5cf147938949e56e7b275a9fc81e6eb
Rafael Cena <rafael.cena@hotmail.com>
2022-11-26 09:21:33 -0300

    acessando galeria

--- src/screens/Profile.tsx
+++ src/screens/Profile.tsx
@@ -1,10 +1,11 @@
 import { ScreenHeader } from '@components/ScreenHeader';
 import { useState } from 'react';
 import { UserPhoto } from '@components/UserPhoto';
-import { Center, ScrollView, VStack, Skeleton ,Text, Heading} from 'native-base';
+import { Center, ScrollView, VStack, Skeleton, Text, Heading } from 'native-base';
 import { TouchableOpacity } from 'react-native';
 import { Input } from '@components/Input';
 import { Button } from '@components/Button';
+import * as ImagePicker from 'expo-image-picker';
 
 
 const PHOTO_SIZE = 33;
@@ -12,6 +13,34 @@ const PHOTO_SIZE = 33;
 export function Profile() {
 
   const [photoIsLoading, setPhotoIsLoading] = useState(false);
+  const [ userPhoto, setUserPhoto] = useState('https://github.com/DevRafaelCena.png')
+
+  async function handleUserPhotoSelected(){
+    setPhotoIsLoading(true);
+
+    try {
+      const photoSelected = await ImagePicker.launchImageLibraryAsync({
+        mediaTypes: ImagePicker.MediaTypeOptions.Images,
+        quality: 1,
+        aspect: [4, 4],
+        allowsEditing: true,
+      });
+
+      if(photoSelected.canceled) {
+        return;
+      }
+
+      if(photoSelected.assets[0].uri) {
+        setUserPhoto(photoSelected.assets[0].uri);
+      }
+
+    } catch (error) {
+      console.log(error)
+    } finally {
+      setPhotoIsLoading(false)
+    }
+  }
+
   return (
     <VStack flex={1}>
       <ScreenHeader title='Perfil' />
@@ -29,13 +58,13 @@ export function Profile() {
               />
             :
               <UserPhoto 
-                source={{ uri: 'https://github.com/DevRafaelCena.png' }}
+                source={{ uri: userPhoto }}
                 alt="Foto do usuário"
                 size={PHOTO_SIZE}
               />
           }
 
-            <TouchableOpacity>
+           <TouchableOpacity onPress={handleUserPhotoSelected}>
               <Text color="green.500" fontWeight="bold" fontSize="md" mt={2} mb={8}>
                 Alterar Foto
               </Text>
